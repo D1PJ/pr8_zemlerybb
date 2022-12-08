f598d427ccf0483dab9b84328f6af3decabbebc5
colleg <colleg@cc.psu>
2022-12-08 18:23:00 +0300

    Расширили алфавит

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import random
 login = input('Login ')
 password = ''
 for x in range(11): #Количество символов (11)
-    password = password + random.choice(list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZабвгдеёжзийклмнопрстуфхцчшщъыьэюя')) #Символы
+    password = password + random.choice(list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZабвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦШЩЪЫЬЭЮЯ')) #Символы
 print('Hello, ', login, 'your password is: ', password)
