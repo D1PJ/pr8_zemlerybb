b04a42d214dbcb9f7327684a318ec92ecc49f131
colleg <colleg@cc.psu>
2022-12-08 18:18:49 +0300

    Написали программу и сделали коммит файла main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import random
+login = input('Login ')
+password = ''
+for x in range(11): #Количество символов (11)
+    password = password + random.choice(list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ')) #Символы
+print('Hello, ', login, 'your password is: ', password)
